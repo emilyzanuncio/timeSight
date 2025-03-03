73c873c30c26b14bfcadbee3617f4a6ef94bc186
Emily <emilyzanun@gmail.com>
2025-03-03 14:55:14 -0400

    Add project files.

---
+++ timeSight/timeSight.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace timeSight
+{
+    partial class timeSight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelNavbar = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelNavbar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelNavbar
+            // 
+            this.panelNavbar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelNavbar.Controls.Add(this.button4);
+            this.panelNavbar.Controls.Add(this.button3);
+            this.panelNavbar.Controls.Add(this.button2);
+            this.panelNavbar.Controls.Add(this.button1);
+            this.panelNavbar.Location = new System.Drawing.Point(330, 480);
+            this.panelNavbar.Name = "panelNavbar";
+            this.panelNavbar.Size = new System.Drawing.Size(340, 90);
+            this.panelNavbar.TabIndex = 0;
+            this.panelNavbar.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panelNavbar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelNavbar_MouseDown);
+            this.panelNavbar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelNavbar_MouseMove);
+            this.panelNavbar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelNavbar_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(260, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 70);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "User";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Timeline";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Calendar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gallery";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timeSight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.panelNavbar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "timeSight";
+            this.Text = "timeSight";
+            this.Load += new System.EventHandler(this.timeSight_Load);
+            this.panelNavbar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelNavbar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ timeSight/timeSight.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timeSight
+{
+    public partial class timeSight : Form
+    {
+        public timeSight()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timeSight_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Navbar position
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panelNavbar.Dock = DockStyle.Bottom; // Default position
+        }
+        #endregion
+
+
+
+        #region snapToEdge
+        private void SnapToMiddle()
+        {
+            int screenWidth = this.ClientSize.Width; // Why is 'this' here? What does it do?
+            int screenHeight = this.ClientSize.Height;
+            int panelWidth = panelNavbar.Width;
+            int panelHeight = panelNavbar.Height;
+
+            // Define distance to edges
+            int leftDistance = panelNavbar.Left;
+            int rightDistance = screenWidth - (panelNavbar.Left + panelWidth);
+            int topDistance = panelNavbar.Top;
+            int bottomDistance = screenHeight - (panelNavbar.Top + panelHeight);
+
+            int minDistance = Math.Min(Math.Min(leftDistance, rightDistance), Math.Min(topDistance, bottomDistance));
+
+            if (minDistance == leftDistance)
+            {
+                //panelNavbar.Dock = DockStyle.Left;
+                panelNavbar.Left = 0;
+                panelNavbar.Top = (screenHeight - panelHeight) / 2;
+            }
+            else if (minDistance == rightDistance)
+            {
+                panelNavbar.Left = screenWidth - panelWidth;
+                panelNavbar.Top = (screenHeight - panelHeight) / 2;
+            }
+            else if (minDistance == topDistance)
+            {
+                panelNavbar.Top = 0;
+                panelNavbar.Left = (screenWidth - panelWidth) / 2;
+            }
+            else
+            {
+                panelNavbar.Top = screenHeight - panelHeight;
+                panelNavbar.Left = (screenWidth - panelWidth) / 2;
+            }
+        }
+
+        #endregion
+
+        #region dragPanel
+        private bool isDragging = false;
+        private Point dragStart;
+
+        private Size originalSize; // Store the original size
+        private Control[] childControls; // Store child controls
+        private void panelNavbar_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            dragStart = e.Location;
+
+            // Store the original size before collapsing
+            originalSize = panelNavbar.Size;
+
+            // Store and hide child controls
+            childControls = panelNavbar.Controls.Cast<Control>().ToArray();
+            foreach (var control in childControls)
+            {
+                control.Visible = false;
+            }
+
+            // Set new collapsed size
+            int collapsedSize = 80; // Diameter of the circular shape
+            panelNavbar.Size = new Size(collapsedSize, collapsedSize);
+            panelNavbar.BackColor = Color.Aqua;
+
+            // Create a circular region
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, collapsedSize, collapsedSize);
+            panelNavbar.Region = new Region(path);
+        }
+
+        private void panelNavbar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                // Move the panel based on the mouse movement
+                this.panelNavbar.Left += e.X - dragStart.X;
+                this.panelNavbar.Top += e.Y - dragStart.Y;
+            }
+        }
+
+        private void panelNavbar_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+
+            // Restore original size
+            panelNavbar.Size = originalSize;
+            panelNavbar.Region = null; // Reset shape
+            panelNavbar.BackColor = SystemColors.Control; // Restore default background
+
+            // Show child controls again
+            foreach (var control in childControls)
+            {
+                control.Visible = true;
+            }
+
+            SnapToMiddle(); // Snap to the middle of each side
+        }
+        #endregion
+    }
+}
